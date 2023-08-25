8332ebbfd63b294f26f9317299cfe3df6b118c28
Senhor57 <143223072+Senhor57@users.noreply.github.com>
2023-08-25 18:08:31 -0300

    Add files via upload
    faz o l agora

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function Perfil() {
+  return (
+    <div>
+    <h1>Rafael Undiciatti</h1>
+    <h2>Tenho 15 anos, acordo entre ás 7:40 e ás 8:20, não fumo nem bebo, todo domingo cozinho macarrão para o almoço, costumo finalizar tarefas na própria escola.</h2>
+  </div>
+    
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+     
+      < Perfil/>
+    </div>
+  );
+}
